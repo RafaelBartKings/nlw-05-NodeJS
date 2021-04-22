6fbd92cc83f7af9708ab5b61da39e8ec6d63dd59
rafaelreis <rafyangtj@gmail.com>
2021-04-21 21:10:04 -0300

    subindo projeto NLW05

---
+++ src/routes.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import { MessagesController } from "./controllers/MessagesController";
+import { SettingsController } from "./controllers/SettingsControlers";
+import { UsersController } from "./controllers/UsersController";
+
+const routes = Router();
+
+const settingsController = new SettingsController();
+const usersController = new UsersController();
+const messagesController = new MessagesController();
+
+routes.post("/settings", settingsController.create);
+
+routes.post("/users", usersController.create);
+
+routes.post("/messages", messagesController.create);
+routes.get("/messages/:id", messagesController.showByUser);
+
+export { routes };
+
+
+/**
+ * Tipos de parametros
+ * Routes Params => Paramentros de rotas
+ * http://localhosta:3333/settings/1
+ * Query Params => Filtros e buscas
+ * http:/localhost:3333/settings/1?search=algumacoisa
+ *  
+ * Body params => {
+ * 
+ * } 
+ */
+
+
---
+++ src/services/UsersService.ts
@@ -0,0 +1,37 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+
+class UsersService {
+    private usersRepository: Repository<User>;
+
+    constructor() {
+        this.usersRepository = getCustomRepository(UsersRepository);
+    }
+
+    async create(email: string) {
+        //Verificar se Usuario existe
+
+        const userExists = await this.usersRepository.findOne({
+            email
+        });
+
+        // Se não existir, salva no DB
+        if(userExists) {
+            return userExists;
+        }
+        
+        const user = this.usersRepository.create({
+            email
+        });
+
+        await this.usersRepository.save(user);
+
+        //Se existir, retornar user
+        return user;
+
+    }
+}
+
+export { UsersService}
