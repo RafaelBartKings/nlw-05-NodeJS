ccfddd5c6f11841f38927cf6684db173d477fd8b
rafaelreis <rafyangtj@gmail.com>
2021-04-24 17:01:07 -0300

    final do projeto

--- src/services/ConnectionsService.ts
+++ src/services/ConnectionsService.ts
@@ -1,5 +1,6 @@
 import { getCustomRepository, Repository } from "typeorm";
 import { Connection } from "../entities/Connection";
+import { Setting } from "../entities/Settings";
 import { ConnectionsRpository } from "../repositories/ConnectionsRepository";
 
 interface IConnectionCreate {
@@ -36,6 +37,35 @@ class ConnectionsService {
 
         return connection;
     }
+
+    async findAllWithoutAdmin() {
+        const connections = await this.connectionsRepository.find({
+            where: { admin_id: null},
+            relations: ["user"]
+        });
+
+        return connections;
+    }
+
+    async findBySocketID(socket_id: string) {
+        const connection  = await this.connectionsRepository.findOne({
+            socket_id
+        });
+
+        return connection;
+    }
+
+    async updateAdminID(user_id: string, admin_id: string) {
+        await this.connectionsRepository
+        .createQueryBuilder()
+        .update(Connection)
+        .set({ admin_id })
+        .where("user_id = :user_id", {
+            user_id,
+        })
+        .execute();
+        
+    }
 };
 
-export { ConnectionsService }
+export { ConnectionsService };
